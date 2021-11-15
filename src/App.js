e878ea399e0f7252a190ec199477ccd04505db06
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2021-11-15 17:49:24 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,64 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [x, setX] = useState(null);
-  useEffect(() => {}, []);
-  const handleClick = () => {
-    window.top.postMessage(
-      JSON.stringify({
-        error: false,
-        message: "foo=bar",
-      }),
-      //! security
-      //only let a certain domain to receive send messages
-      "*"
-    );
-  };
-  function getCookie(name) {
-    var dc = document.cookie;
-    var prefix = name + "=";
-    var begin = dc.indexOf("; " + prefix);
-    if (begin == -1) {
-      begin = dc.indexOf(prefix);
-      if (begin != 0) return null;
-    } else {
-      begin += 2;
-      var end = document.cookie.indexOf(";", begin);
-      if (end == -1) {
-        end = dc.length;
+  // const handleClick = () => {
+  //   window.top.postMessage(
+  //     JSON.stringify({
+  //       error: false,
+  //       message: "foo=bar",
+  //     }),
+  //     //! security
+  //     //only let a certain domain to receive send messages
+  //     "*"
+  //   );
+  // };
+
+  const handleClick = async () => {
+    const data = await axios.post(
+      "https://pini-backend-playground.herokuapp.com/test",
+      {
+        hi: "hello",
       }
-    }
-    // if (decodeURI(dc.substring(begin + prefix.length, end))) {
-    //   setX("x");
-    // }
-    // because unescape has been deprecated, replaced with decodeURI
-    //return unescape(dc.substring(begin + prefix.length, end));
-    return decodeURI(dc.substring(begin + prefix.length, end));
-  }
-
-  function createCookie() {
-    document.cookie = "foo=bar; SameSite=none; Secure";
-    setX("x");
-  }
-  const handleLogin = () => {
-    createCookie();
-  };
-
-  const checkCookies = () => {
-    if (getCookie("foo")) {
-      setX("x");
-    }
-    console.log("cookies inside iframe is:", document.cookie);
+    );
+    console.log(data);
   };
 
   return (
     <div className="App">
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleClick}>Send post message</button>
-      <button onClick={checkCookies}> Check cookies</button>
-      {getCookie("foo") && <h2>You are logged in</h2>}
+      <button onclick={handleClick}>Click me</button>
     </div>
   );
 }
